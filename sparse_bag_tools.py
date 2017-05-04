a301387eeec64dbb1264d386d98e83a1edba5927
sar516 <stormaross@gmail.com>
2017-05-03 20:14:54 -0400

    updated bag tools

--- sparse_bag_tools.py
+++ sparse_bag_tools.py
@@ -7,7 +7,7 @@ import random
 
 def bag_of_games(data_set):
     user_ids = data_set["user_id"].unique()
-    user_bag = {}#.fromkeys(map((lambda x: str(x)),user_ids), {})
+    user_bag = {}
     
     
     for user in user_ids:
@@ -59,51 +59,78 @@ def leave_n_in(bag, n=1, p = .25):
             query_bag[user] = copy_bag.pop(user)
     return copy_bag, removed, query_bag
     
-    
-    
 
-def baseline(train_bag, query_bag, heldout, user_reg=25, game_reg=25):
-    total_bag = copy.deepcopy(train_bag)
-    total_bag.update(query_bag)
-    n = 0
-    total = 0
-    for userid in train_bag:
-        gamelist = train_bag[userid]
-        n += len(list(gamelist.keys()))
-        total += sum(gamelist.values())
+def mean_norm(bag, n = 1, p = .25):
+    train_bag, test_points, query_bag = leave_n_in(bag, n, p)
+    mean = 0
+    N = 0
+    for user in train_bag.keys():
+        games_list = train_bag[user]
+        mean += sum(games_list.values())
+        N += len(games_list)
     else:
-        total_avg = total/n
+        mean = mean/N
         
-    user_baseline = {}
-    for userid in total_bag:
-        user = total_bag[userid]
-        user_baseline[userid] = (sum(user.values()) - len(user)*total_avg)/(len(user)+user_reg)
+    for user in train_bag.keys():
+        games_list = train_bag[user]
+        for game in games_list.keys():
+            games_list[game] = games_list[game]/mean
     
-    predictions = {}
-    for test_point in heldout.keys():
-        test_user = test_point[0]
-        test_game = test_point[1]
-        bu = user_baseline[test_user]
-        n = 0
-        bi = 0
-        for userid in train_bag:
-            user = train_bag[userid]
-            if test_game in user.keys():
-                bi += user[test_game] - user_baseline[userid] - total_avg
-                n += 1
-        else:
-            bi = bi/(n + game_reg)
+    for pair in test_points.keys():
+        test_points[pair] = test_points[pair]/mean
         
-        
-        predictions[test_point] = total_avg + bu + bi
-    return predictions
+    return train_bag, test_points, query_bag 
+    
 
+class Baseline():
+    
+    def __init__(self, reg = 25):
+        self.reg = reg
+        self.fitted = False
+        
+    def get_bu(self, users_list):
+        bu = (sum([rating  - self.mu  for rating in users_list.values()])/
+              (len(users_list)+self.reg))
+        return bu
+    
+    def get_bi(self, items_list):
+        bi = (sum([rating - self.user_baselines[user] - self.mu
+                   for user, rating in items_list.items()])/(len(items_list)+self.reg))
+        return bi
+    
+    
+    def fit(self, train_bag):
+        self.train_bag = train_bag
+        self.item_bag  = sparse_bag_transpose(train_bag)
+        self.mu = (sum([sum(v.values()) for v in self.train_bag.values()])/
+                   sum([len(v.keys()) for v in self.train_bag.values()]))
+        self.user_baselines = {user : self.get_bu(users_list) 
+                               for user, users_list in self.train_bag.items()}
+        self.item_baselines = {item : self.get_bi(items_list)
+                               for item, items_list in self.item_bag.items()}
+        
+        self.fitted = True
+        
+        return self
+    
+    
+    def predict(self, test_points, query_bag):
+        predictions = {}
+        query_baselines = {user : self.get_bu(users_list) 
+                           for user, users_list in query_bag.items()}
+        for pair in test_points:
+            user, item = pair
+            b_u = query_baselines[user]
+            b_i = self.item_baselines.get(item, 0)
+            predictions[pair] = self.mu + b_u + b_i
+                
+        return predictions
 
 def get_metrics(actuals, preds):
     abs_diff = {}
     square_diff = {}
     for point in preds.keys():
-        diff = float(actuals[point]) - float(preds[point])
+        diff = actuals[point] - preds[point]
         abs_diff[point] = abs(diff)
         square_diff[point] = diff*diff
     MAE = np.array(list(abs_diff.values())).mean()
@@ -115,8 +142,61 @@ def sparse_bag_transpose(bag):
     transpose = {}
     for user, games_list in bag.items():
         for game, rating in games_list.items():
-            if game not in transpose:
+            if game not in transpose.keys():
                 transpose[game] = {user : rating}
             else:    
                 transpose[game].update({user : rating})
     return transpose
+
+def dict_dist(dict1, dict2):
+    keys1 = list(dict1.keys())
+    keys2 = list(dict2.keys())
+    all_keys = list(set(keys1) | set(keys2))
+    dist = 0
+    for key in all_keys:
+        dist += (dict1.get(key,0)-dict2.get(key,0))**2
+        
+    return dist**.5
+
+
+def knn_dict(neighbors, query, k = 2):
+    keys = list(neighbors.keys())
+    dists = np.zeros(len(neighbors))
+    knn = {}
+    if k > len(keys):
+        raise ValueError("k greater than number of training items")
+        
+    for i in range(len(keys)):
+        
+        key = keys[i]
+        dists[i] = dict_dist(neighbors[key], query)
+        
+    for j in range(k):
+        
+        m = np.argmin(dists)
+        dists[m] = 10**10
+        selected = keys[m] 
+        knn[selected] = neighbors[selected]
+        
+    return knn
+
+def dict_dot(dict1, dict2):
+    keys1 = list(dict1.keys())
+    keys2 = list(dict2.keys())
+    cross_keys = list(set(keys1) & set(keys2))
+    prod = 0
+    if len(cross_keys) == 0:
+        return prod
+    else:
+        for key in cross_keys:
+            prod += dict1[key]*dict2[key]
+        
+        return prod
+    
+    
+    
+def cos_sim(dict1, dict2):
+    try:
+        return dict_dot(dict1, dict2)/(dict_dist(dict1,{})*dict_dist(dict2,{}))
+    except ZeroDivisionError:
+        return 0
